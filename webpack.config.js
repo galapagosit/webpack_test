4e96989bbad7eb68d512487273a3025d884c5a0f
Daisuke Hashimoto <d_hashimoto@binc.jp>
2015-12-29 19:35:10 +0900

    webpack test

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+  },
+  watch: true,
+  stats: {
+    colors: true
+  },
+  progress: true,
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jQuery",
+        "windows.jQuery": "jquery"
+    }),
+  ]
+};
