2b9adc362f6373f8b5435ed30eb14b19389870b6
Daisuke Hashimoto <d_hashimoto@binc.jp>
2016-01-05 11:01:28 +0900

    webpack test

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-
-$(function(){
-  console.log('hoge');
-  console.log(jQuery('#hoge'));
-        $('#postcode1').jpostal({
-        postcode : [
-          '#postcode1',
-        ],
-        address : {
-          '#address1'  : '%3',
-          '#address2'  : '%4',
-          '#address3'  : '%5'
-        }
-      });
+$(function () {
+  $('#postcode1').jpostal({
+    postcode: [
+      '#postcode1',
+    ],
+    address: {
+      '#address1': '%3',
+      '#address2': '%4',
+      '#address3': '%5',
+      '#address1_kana': '%8',
+      '#address2_kana': '%9',
+      '#address3_kana': '%10',
+    }
+  });
 });
